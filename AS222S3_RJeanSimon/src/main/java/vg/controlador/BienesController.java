cae555220d8090999e35b8db8f7c1c304057cc11
Jean Simon <jean.simon@vallegrande.edu.pe>
2023-12-18 09:56:03 -0500

    Creacion del front end con la lista

--- AS222S3_RJeanSimon/src/main/java/vg/controlador/BienesController.java
+++ AS222S3_RJeanSimon/src/main/java/vg/controlador/BienesController.java
@@ -5,6 +5,7 @@ import vg.model.Bienes;
 import vg.service.BienesService;
 import vg.service.IBienesService;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,6 +23,7 @@ public class BienesController extends HttpServlet {
         this.bienesService = new BienesService();
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("application/json");
@@ -36,56 +38,4 @@ public class BienesController extends HttpServlet {
             e.printStackTrace();
         }
     }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String action = request.getParameter("action");
-
-        try {
-            if ("update".equals(action)) {
-                // Procesar la actualización
-                int bienesId = Integer.parseInt(request.getParameter("bienesId"));
-                Bienes bien = new Bienes();
-                bien.setBienesid(bienesId);
-                bien.setCantidad(request.getParameter("cantidad"));
-                bien.setDetalle(request.getParameter("detalle"));
-                bien.setValorlibro(request.getParameter("valorlibro"));
-                bien.setFecha_ingreso(request.getParameter("fecha_ingreso"));
-                bien.setFecha_depreciacion(request.getParameter("fecha_depreciacion"));
-                bien.setStatus(request.getParameter("status"));
-                bienesService.update(bien);
-                request.getSession().setAttribute("successMessage", "Bien actualizado con éxito.");
-            } else if ("delete".equals(action)) {
-                // Proceso de eliminar
-                String bienesIdStr = request.getParameter("bienesid");
-                if (bienesIdStr != null && !bienesIdStr.isEmpty()) {
-                    int bienesId = Integer.parseInt(bienesIdStr);
-                    Bienes bien = new Bienes();
-                    bien.setBienesid(bienesId);
-                    bienesService.delete(bien);
-                    request.getSession().setAttribute("successMessage", "Bien eliminado con éxito.");
-                } else {
-                    request.getSession().setAttribute("errorMessage", "ID del bien no proporcionado.");
-                }
-            } else {
-                // Procesar la inserción
-                Bienes bien = new Bienes();
-                bien.setCantidad(request.getParameter("cantidad"));
-                bien.setDetalle(request.getParameter("detalle"));
-                bien.setValorlibro(request.getParameter("valorlibro"));
-                bien.setFecha_ingreso(request.getParameter("fecha_ingreso"));
-                bien.setFecha_depreciacion(request.getParameter("fecha_depreciacion"));
-                bien.setStatus(request.getParameter("status"));
-                // Insertar bien
-                bienesService.insert(bien);
-                request.getSession().setAttribute("successMessage", "Bien registrado con éxito.");
-            }
-            response.sendRedirect("Vista/Bienes/bienesList.jsp"); // Redirige a la lista de bienes
-        } catch (Exception e) {
-            e.printStackTrace();
-            request.getSession().setAttribute("errorMessage", "Error en la operación: " + e.getMessage());
-            response.sendRedirect("Vista/Bienes/bienesError.jsp"); // Redirige a la página de error
-        }
-    }
-
 }
